e45e07ff1926344d149b2fe9a0fc2bb79704e4f4
Alexander Panasik <rastpk@gmail.com>
2022-02-20 14:25:13 +0300

    new branch

--- src/pages/lastpage/Lastpage.js
+++ src/pages/lastpage/Lastpage.js
@@ -3,8 +3,10 @@ import Smallheader from "../../components/smallheader/Smallheader";
 import { contentData } from '../../components/data/contentData';
 import Coffeelist from "../../components/coffeelist/Coffeelist";
 import Footer from "../../components/footer/Footer";
+import { coffeeData } from '../../components/data/coffeeData';
 
 function Lastpage(props) {
+
     return (
         <>
             <Smallheader bg={'for-your'} title={'For Your Pleasure'} />
@@ -12,7 +14,7 @@ function Lastpage(props) {
                 title={contentData[1].title}
                 subtitle={contentData[1].subtitle}
                 suptitle={contentData[1].suptitle} />
-            <Coffeelist />
+            <Coffeelist data={coffeeData} />
             <Footer />
         </>
     );
